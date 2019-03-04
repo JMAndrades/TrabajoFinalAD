25ed25e84286517172c1f3f7861b6796b95f44e0
Jose Mari <yusei625@gmail.com>
2019-03-04 12:12:26 +0100

    Creacion de la promera parte del proyecto

---
+++ index.js
@@ -0,0 +1,18 @@
+const {Client} = require('pg');
+
+const client = new Client({
+    user: "postgres",
+    password: "12345",
+    host: "postgres",
+    port: 5432,
+    database: "Clients"
+});
+
+
+client.connect()
+.then(() => { console.log('Conectado correctamente'); })
+.then(() => client.query("INSERT INTO Clients values ($1, $2, $3, $4)", [100, 'Jhon', 'USA', '123456789']))
+.then(() => client.query("SELECT * from Clients"))
+.then(results => console.table(results.rows))
+.catch(e => console.log)
+.finally(() => client.end());
